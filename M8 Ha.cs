3087fd51dd96c04f7597ee3644e445ca09d70183
deoxysjr <deoxysjr@gmail.com>
2017-05-16 09:54:16 +0200

    Update M8 Ha.cs

--- M8 Ha.cs
+++ M8 Ha.cs
@@ -1,1 +1,8 @@
-
+command.CreateCommand("rollDice")
+        .Description("Rolls a random Number between Min and Max")
+        .Parameter("Min")
+        .Parameter("Max")
+        .Do(async e =>
+        {
+            await e.Channel.SendMessage($"[{random.Next(int.Parse(e.Args[0]), int.Parse(e.Args[1]) + 1)}]);
+        });
