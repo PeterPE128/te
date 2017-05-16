ef75944bd160f452ca2f7289b999c06eb670de4e
deoxysjr <deoxysjr@gmail.com>
2017-05-16 11:38:40 +0200

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,1 +1,8 @@
-
+if(CommandsUsed >= 0)
+{
+    var path = @"memes\lol.txt";
+    var line = "1";//haha
+    var CommandsUsed1 = File.readLines(path, line);
+    CommandsUsed1++
+    File.writeLine(path, $"{CommandsUsed1}$");
+}
