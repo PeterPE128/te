1d565d9f2a2469202a472cb36485e314ba1a68f4
deoxysjr <deoxysjr@gmail.com>
2017-05-16 16:56:35 +0200

    Update exit.cs

--- exit.cs
+++ exit.cs
@@ -1,9 +1,18 @@
 commands.CreateCommand("close")
 .Do(async (e) =>
 {
-  if("e.User.Id == "id")
+  if(e.User.Id == 245140333330038785)
      {
-        await e.Channel.SendMessage($"{e.User} Super Bot is stoping");  await e.Channel.SendMessage("confirm stop in the consol");  Consol.WriteLine("Do you want to stop the bot");  Consol.WriteLine("Yes or No");    if(Consol.ReadLine() == "Yes")    {      await e.Channel.SendMessage("The bot has been stoped");      discord.Disconect();      Console.WriteLine("The Bot has been Disconected");    }
+        await e.Channel.SendMessage($"{e.User} Super Bot is stoping");
+        await e.Channel.SendMessage("confirm stop in the consol");
+        Consol.WriteLine("Do you want to stop the bot");
+        Consol.WriteLine("Yes or No");
+        if(Consol.ReadLine() == "Yes")
+        {
+            await e.Channel.SendMessage("The bot has been stoped");
+            discord.Disconect();
+            Console.WriteLine("The Bot has been Disconected");
+        }
      }
   
 });
