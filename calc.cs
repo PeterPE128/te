d9c7d7204db9f818afb35c6c5808c711ed7ac26d
deoxysjr <deoxysjr@gmail.com>
2017-05-16 13:21:59 +0200

    Update calc.cs

--- calc.cs
+++ calc.cs
@@ -1,1 +1,22 @@
-
+command.CreateCommand("reken")
+    .parameter("ags")
+    .Do(async (e) =>
+    {
+    if(Args[0] == "")
+    {
+        e.Channel.SendMessage("You need number");
+    }
+    if(Args[1] == "")
+    {
+        e.Channel.SendMessage("You need a seperator");
+    }
+    if(Args[2] == "")
+    {
+        e.Channel.SendMessage("You need a second number");
+    }
+    if(Args[1] == "+")
+    {
+    var ant = Args[0] ++ Args[2];
+    await e.Channel.SendMessage($"Your awnser is {ant}"
+    }
+    });
