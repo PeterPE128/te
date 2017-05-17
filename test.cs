8241a82e2f72f3236eed92c5fdf1387f7bc9c911
deoxysjr <deoxysjr@gmail.com>
2017-05-17 08:46:52 +0200

    Update test.cs

--- test.cs
+++ test.cs
@@ -5,6 +5,7 @@ if(CommandsUsed >= 0)
     var CommandsUsed1 = File.readLines(path, line);
     CommandsUsed1++
     File.writeLine(path, $"{CommandsUsed1}");
+    Console.WriteLine($"ther are now {CommandsUsed1} used");
     CommandsUsed = 0;
 }
 else
